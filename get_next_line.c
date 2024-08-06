0523c0fa2d2912cb1b290a19c46788d8934a669e
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2024-08-06 14:27:51 +0200

    redistribucion de código

--- get_next_line.c
+++ get_next_line.c
@@ -10,55 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-#endif
+#include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	lenght;
-
-	lenght = 0;
-	if (!str)
-		return (lenght);
-	while (str[lenght])
-		lenght++;
-	return (lenght);
-}
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned char	*s;
-	unsigned char	*d;
-
-	s = (unsigned char *)src;
-	d = (unsigned char *)dest;
-	while (n--)
-		*d++ = *s++;
-	return (dest);
-}
-
-char	*ft_strdup(const char *str)
-{
-	size_t	size;
-	char	*dup;
-
-	if (!str)
-		return (NULL);
-	size = ft_strlen(str) + 1;
-	dup = (char *)malloc(size * sizeof(char));
-	if (!dup)
-		return (NULL);
-	ft_memcpy(dup, str, size);
-	return (dup);
-}
-
-void	*ft_calloc(void ptr, size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t			t_size;
 	void			*arr;
@@ -84,8 +38,6 @@ void	*ft_calloc(void ptr, size_t nmemb, size_t size)
 void	*ft_realloc(void *ptr, size_t new_size)
 {
 	void	*new_ptr;
-	size_t	size_old;
-	size_t	size_cpy;
 
 	if (!new_size)
 	{
@@ -94,58 +46,19 @@ void	*ft_realloc(void *ptr, size_t new_size)
 	}
 	new_ptr = ft_calloc(new_size, sizeof(char));
 	if (!new_ptr)
-	{
-		free(ptr);
 		return (NULL);
-	}
 	if (ptr)
 	{
-		size_old = ft_strlen(ptr) + 1;
-		if (size_old < new_size)
-			size_cpy = size_old;
+		if (ft_strlen(ptr) + 1 < new_size)
+			ft_memcpy(new_ptr, ptr, ft_strlen(ptr) + 1);
 		else
-			size_cpy = new_size;
-		ft_memcpy(new_ptr, ptr, size_cpy);
+			ft_memcpy(new_ptr, ptr, new_size);
 		free(ptr);
 	}
 	return (new_ptr);
 }
 
-char	*ft_strchr(const char *str, int c)
-{
-	while (*str)
-	{
-		if (*str == (char)c)
-			return ((char *)str);
-		str++;
-	}
-	if (*str == (char)c)
-		return ((char *)str);
-	return (NULL);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	size_t	t_size;
-	size_t	s1_size;
-
-	if (!s1 && !s2)
-		return (NULL);
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (s1);
-	s1_size = ft_strlen(s1);
-	t_size = s1_size + ft_strlen(s2) + 1;
-	s1 = (char *)ft_realloc(s1, t_size * sizeof(char));
-	if (!s1)
-		return (NULL);
-	while (*s2)
-		s1[s1_size++] = *s2++;
-	return (s1);
-}
-
-char	**buffer_check_nl(char **buffer, unsigned int buffer_ch)
+static char	**buffer_check_nl(char **buffer, unsigned int buffer_ch)
 {
 	buffer[3] = ft_strchr(buffer[buffer_ch], '\n');
 	if (buffer[3])
@@ -168,7 +81,7 @@ char	**buffer_check_nl(char **buffer, unsigned int buffer_ch)
 	return (buffer);
 }
 
-char	**get_new_buffer(int fd, char **buffer)
+static char	**get_new_buffer(int fd, char **buffer)
 {
 	if (buffer[2])
 	{
---
+++ get_next_line_utils.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 13:55:28 by alejhern          #+#    #+#             */
+/*   Updated: 2024/08/06 13:55:31 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	lenght;
+
+	lenght = 0;
+	if (!str)
+		return (lenght);
+	while (str[lenght])
+		lenght++;
+	return (lenght);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*s;
+	unsigned char	*d;
+
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
+
+char	*ft_strdup(const char *str)
+{
+	size_t	size;
+	char	*dup;
+
+	if (!str)
+		return (NULL);
+	size = ft_strlen(str) + 1;
+	dup = (char *)malloc(size * sizeof(char));
+	if (!dup)
+		return (NULL);
+	ft_memcpy(dup, str, size);
+	return (dup);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if (*str == (char)c)
+		return ((char *)str);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	t_size;
+	size_t	s1_size;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (s1);
+	s1_size = ft_strlen(s1);
+	t_size = s1_size + ft_strlen(s2) + 1;
+	s1 = (char *)ft_realloc(s1, t_size * sizeof(char));
+	if (!s1)
+		return (NULL);
+	while (*s2)
+		s1[s1_size++] = *s2++;
+	return (s1);
+}
